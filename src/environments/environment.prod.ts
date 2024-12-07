12f536fdf6d49427eef535ea0c9ddebba69bd156
ubaidh <obaidh@live.com>
2024-12-07 11:53:55 +0530

    uo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://49.50.85.245:8911/api/v1',
-  acogsApiUrl: 'http://49.50.85.245:8912/api',
-  folderPath: 'http://49.50.85.245:8911',
+  apiUrl: 'http://49.50.85.245:8912/api/v1',
+  acogsApiUrl: 'http://49.50.85.245:8911/api',
+  folderPath: 'http://49.50.85.245:8912',
 };
